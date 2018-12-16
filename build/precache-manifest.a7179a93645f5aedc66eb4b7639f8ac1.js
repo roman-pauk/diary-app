307e51c2bdb87cadc22fa46f988ba6b21bb5a0db
Roman Pauk <pauk83258325@gmail.com>
2018-12-16 16:23:22 +0200

    build the app

---
+++ build/precache-manifest.a7179a93645f5aedc66eb4b7639f8ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dac091f9470218144540",
+    "url": "/static/js/main.dac091f9.chunk.js"
+  },
+  {
+    "revision": "7242a92b1ec8545871a9",
+    "url": "/static/js/1.7242a92b.chunk.js"
+  },
+  {
+    "revision": "dac091f9470218144540",
+    "url": "/static/css/main.4730c498.chunk.css"
+  },
+  {
+    "revision": "7242a92b1ec8545871a9",
+    "url": "/static/css/1.4daaa3df.chunk.css"
+  },
+  {
+    "revision": "1f31a89460e47fa491cea9d7f3ff3eb5",
+    "url": "/index.html"
+  }
+];
